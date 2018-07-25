0da9611b2c0683f6c194554653751939cf8cfdf4
piekaa <piekoszek@gmail.com>
2018-07-25 20:27:59 +0200

    slider (#2)

---
+++ Assets/Scripts/PiekaSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PiekaSlider : MonoBehaviour {
+
+	[SerializeField]
+	private float minValue = 0;
+
+	[SerializeField]
+	private float maxValue = 1;
+
+	private Slider slider;
+	private Text text;
+
+	public float Value { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponentInChildren<Slider> ();
+		text = GetComponentInChildren<Text> ();
+		slider.minValue = minValue;
+		slider.maxValue = maxValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Value = slider.value;
+		text.text = Value.ToString("0.00");
+
+	}
+}
