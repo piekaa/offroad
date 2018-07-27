0a874c9aa0abd3b85dd6b7427a40b435d4b62d92
pieka <qqqqq_12@o2.pl>
2018-07-27 19:20:17 +0200

    Drop button

---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour {
+ 
+	private SpriteRenderer spriteRenderer;
+
+	public GameObject Spawnable;
+	public float Angle;
+
+	void Start () {
+		spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
+		spriteRenderer.color = Color.clear;
+	}
+
+	public void Spawn() {
+
+		Spawnable.transform.position = spriteRenderer.transform.position;
+
+	}
+}
