1ef54b6e4c7cad0fb42b4f33ea317a51d10afe97
piekaa <piekoszek@gmail.com>
2018-07-28 16:50:35 +0200

    Drop button (#3)
    * Drop button
    
    * pr changes

---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour {
+ 
+	private SpriteRenderer spriteRenderer;
+
+	public GameObject Spawnable; 
+
+	void Start () {
+		spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
+		spriteRenderer.color = Color.clear;
+	}
+
+	public void Spawn() {
+
+		Spawnable.transform.position = spriteRenderer.transform.position;
+		Spawnable.transform.rotation = transform.rotation = Quaternion.identity;
+
+	}
+}
