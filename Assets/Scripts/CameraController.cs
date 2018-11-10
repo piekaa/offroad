47676ab2142fcb95a8c859710d4e9acb004ccddb
Piotr Iwaniuk <piekoszek@gmail.com>
2018-11-10 12:37:46 +0100

    Flash on reset

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraController : Resetable
 {
+
+    [SerializeField]
+    private SpriteRenderer curtain;
+
+    [SerializeField]
+    private GameObject followed;
+
+    float resetDelta = 0;
+
     void LateUpdate()
     {
-        Camera.main.transform.rotation = Quaternion.identity;
+        Camera.main.transform.position = new Vector3(followed.transform.position.x, followed.transform.position.y);
+        curtain.transform.position = new Vector3(followed.transform.position.x, followed.transform.position.y, 1);
     }
+
+    public override void Reset()
+    {
+        resetDelta = -0.03f;
+        curtain.color = new Color(curtain.color.r, curtain.color.b, curtain.color.g, 1);
+    }
+
+    void FixedUpdate()
+    {
+        if (resetDelta != 0)
+        {
+            curtain.color = new Color(curtain.color.r, curtain.color.b, curtain.color.g, curtain.color.a + resetDelta);
+            if (curtain.color.a <= 0)
+            {
+                resetDelta = 0;
+            }
+        }
+    }
+
 }
