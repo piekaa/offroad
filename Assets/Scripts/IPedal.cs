19f4acc8bf9465d59f1f713e8d7c822f403a8d2e
Piotr Iwaniuk <piekoszek@gmail.com>
2018-11-10 16:52:49 +0100

    Pr change

--- Assets/Scripts/IPedal.cs
+++ Assets/Scripts/IPedal.cs
@@ -2,7 +2,6 @@ public delegate void OnIsPressed(float value);
 
 public interface IPedal
 {
-    //todo Add On Pressed
     float Value { get; }
 
     void RegisterOnIsPressed(OnIsPressed onIsPressed);
