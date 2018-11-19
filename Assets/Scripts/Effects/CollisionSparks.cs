13e5cd4e7cc3c6b4eaffaaf514bdffc405b5eb04
Piotr Iwaniuk <piekoszek@gmail.com>
2018-11-19 21:58:21 +0100

    sparks first part

---
+++ Assets/Scripts/Effects/CollisionSparks.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pieka.Effects
+{
+    public class CollisionSparks : MonoBehaviour
+    {
+        public ParticleSystem SparksParticlePrefab;
+
+        private ParticleSystem sparksParticle;
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (sparksParticle == null)
+            {
+                sparksParticle = Instantiate(SparksParticlePrefab);
+                sparksParticle.transform.position = col.GetContact(0).point;
+                sparksParticle.transform.parent = transform;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D other)
+        {
+            if (sparksParticle != null)
+            {
+                Destroy(sparksParticle.gameObject);
+            }
+        }
+
+    }
+}
