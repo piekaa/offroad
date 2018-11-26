ed63e6c3aaa71ab9d8fd9f3bee05791170bfe7ba
piekaa <piekoszek@gmail.com>
2018-11-26 17:07:59 +0100

    particle shader improved (#41)

---
+++ Assets/Scripts/Ui/Number.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pieka.Ui
+{
+    /// <summary>
+    /// Just for type purpose
+    /// </summary>
+    public class Number : MonoBehaviour
+    {
+
+    }
+}
